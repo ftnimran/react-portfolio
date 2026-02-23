0a53728cf96fe0f5cfeffeb980cebdbcc9edc77f
Imran Ali <109467587+ftnimran@users.noreply.github.com>
2026-02-23 23:58:58 +0530

    Add Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar.jsx";
 import Hero from "./pages/Hero.jsx";
 import About from "./pages/About.jsx";
 import Skills from "./pages/Skills.jsx";
+import Projects from "./pages/Projects.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <Projects />
       <Footer />
     </>
   );
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+   <section id='Projects' className='w-screen h-screen pt-17 px-[10%] pb-0'>
+
+     
+
+    </section>
+  )
+}
+
+export default Projects
