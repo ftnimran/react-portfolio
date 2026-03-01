98a9610eaed994d1f37b5a2c2ec5ce57eaedf7d8
Imran Ali <109467587+ftnimran@users.noreply.github.com>
2026-03-01 22:52:22 +0530

    fix padding

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SectionTitle = ({ titleText1, titleText2 }) => {
   return (
     <div className="section-title">
-      <h2 className="pt-2 text-center font-bold text-[45px]">
+      <h2 className="pt-1.5 text-center font-bold text-[25px] md:text-[45px] leading-tight">
         {titleText1}
         <span className="text-[#0ef]">{titleText2}</span>
       </h2>
